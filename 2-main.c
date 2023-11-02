61d4b9956afb377179411448bd9ce9fbcbfd5efb
MwendeHarriet <mwendeharriet@gmail.com>
2023-11-01 23:48:45 -0700

    insert right child

--- 2-main.c
+++ 2-main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
- /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	binary_tree_t *root;
@@ -16,8 +16,8 @@ int main(void)
 	root->right = binary_tree_node(root, 402);
 	binary_tree_print(root);
 	printf("\n");
-	binary_tree_insert_left(root->right, 128);
-	binary_tree_insert_left(root, 54);
+	binary_tree_insert_right(root->left, 54);
+	binary_tree_insert_right(root, 128);
 	binary_tree_print(root);
 	return (0);
 }
