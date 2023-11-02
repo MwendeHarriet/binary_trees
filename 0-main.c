70b27dd23c44f5da358df45990bc6f0c863271b4
MwendeHarriet <mwendeharriet@gmail.com>
2023-11-01 23:56:33 -0700

    delete entire binary tree

--- 0-main.c
+++ 0-main.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	binary_tree_t *root;
-	
+
 	root = binary_tree_node(NULL, 98);
-	
+
 	root->left = binary_tree_node(root, 12);
 	root->left->left = binary_tree_node(root->left, 6);
 	root->left->right = binary_tree_node(root->left, 16);
